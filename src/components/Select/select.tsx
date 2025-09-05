ba5d542dff05986761d6177df7914e2e3a6373b6
whiteduke <zef1rflis@yandex.ru>
2025-09-05 23:56:29 +0500

    feat: update version

--- src/components/Select/select.tsx
+++ src/components/Select/select.tsx
@@ -1,29 +1,31 @@
-import {useState } from 'react'
-import styles from './select.module.css'
+import { useState } from "react";
+import styles from "./select.module.css";
 
 interface SelectProps {
-	title: string
-	childrens: string[]
+  title: string;
+  childrens: string[];
 }
 
 //можно много чего допилить
 export const Select = (props: SelectProps) => {
-	const title = props.title
-	const childrens = props.childrens
-	const [visible, setVisible] = useState<boolean>(false)
+  const title = props.title;
+  const childrens = props.childrens;
+  const [visible, setVisible] = useState<boolean>(false);
 
-	return (
-		<div className={styles.container}>
-			<button className={styles.open_btn} onClick={() => setVisible(!visible)}>{title}</button>
-			{visible && 
-				<div className={styles.childrens}>
-					{childrens.map((child, index) => (
-						<div key={index} className={styles.child}>
-							{child}
-						</div>
-					))}
-				</div>
-			}
-		</div>
-	)
-}
+  return (
+    <div className={styles.container}>
+      <button className={styles.open_btn} onClick={() => setVisible(!visible)}>
+        {title}
+      </button>
+      {visible && (
+        <div className={styles.childrens}>
+          {childrens.map((child, index) => (
+            <div key={index} className={styles.child}>
+              {child}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
